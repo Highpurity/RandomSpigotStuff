344c44628784d4a4ccaf5a60c46d6a5dc5fca639
Highpurity <development@highpurity.cc>
2023-06-07 01:15:08 -0400

    baseplate for toggle - last project tonight

---
+++ src/main/java/highpurity/randomness/commands/insanity.java
@@ -0,0 +1,13 @@
+package highpurity.randomness.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class insanity implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        return true;
+    }
+}
