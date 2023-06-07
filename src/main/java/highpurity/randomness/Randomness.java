cfec6accea24e54a256f4505c1cfa2f0ff1c8d64
Highpurity <development@highpurity.cc>
2023-06-07 01:23:55 -0400

    Attempt #2? (not a genius I forgot to declare the command???)

--- src/main/java/highpurity/randomness/Randomness.java
+++ src/main/java/highpurity/randomness/Randomness.java
@@ -1,5 +1,6 @@
 package highpurity.randomness;
 
+import highpurity.randomness.commands.insanity;
 import highpurity.randomness.events.PlayerGravityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,6 +10,8 @@ public final class Randomness extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new PlayerGravityEvent(), this);
+        getCommand("insanity").setExecutor(new insanity(), this);
+
     }
 
     @Override
